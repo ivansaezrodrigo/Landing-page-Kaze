6d9b68006eaba7ea8ff1432c65cf6825ed486526
Ivan Saez <ivansaezrodri@gmail.com>
2023-01-26 21:45:51 +0100

    Menú responsive

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,22 @@ setInterval(() => {
     setTimeout(() => {
         clouds.classList.remove(cloudsDistortion);
     }, 500);
-    }
-, Math.random() * 10000 + 5000);
+}, Math.random() * 10000 + 5000);
 
+const nav = document.querySelector('nav');
+const enlaces = document.querySelector('.enlaces');
+let abierto = false;
+
+nav.addEventListener('click', () => {
+    // En el caso de que el media query se cumpla, se ejecuta el código
+
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        if (!abierto) {
+            enlaces.style = 'display: flex';
+            abierto = true;
+        } else {
+            enlaces.style.display = 'none';
+            abierto = false;
+        }
+    }
+});
